abbfbe03bc94256e610380cb64369392b3e87d80
Nils Noreyson <NilsNoreyson@gmail.com>
2015-03-08 21:59:16 +0100

    lets see

--- Flask2.py
+++ Flask2.py
@@ -8,49 +8,16 @@ Created on Tue May 13 14:28:23 2014
 
 from flask import Flask,jsonify,request, make_response
 import os
-from mpd_comm import *
-import json
-import threading
-import pickle
-import datetime
+import subprocess, shlex
 
-fileName="/opt/phonebook/phonebook"
-
-telefonBuch={9: 'Toystore',
-             3: 'anton',
-             1: 'Stan',
-             7: 'Kalkbrenner',
-             2: 'Hit Box',
-             4: 'The Katie Melua Collection',
-             6: 'IRM',
-             5: 'Anthems of All',
-             8: 'Broken Bells',
-             0: 'Another Self Portrait',
-             145:'Extrawelt',
-             333:'Streets',
-
-            }
-playlists=[]
-
-def save_to_pickle():
-    pickle.dump(telefonBuch, open( fileName+'.pkl', "wb" ) )
-    date_str=datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')
-    pickle.dump(telefonBuch, open( fileName+"_"+date_str+'_.pkl', "wb" ) )
-    return
-
-
-def load_phoneBook():
-    telefonBuch=pickle.load( open( fileName+'.pkl', "rb" ) )
-    return telefonBuch
-
-
-#save_to_pickle()
-telefonBuch=load_phoneBook()
 
 topPath=os.path.dirname(os.path.realpath(__file__))
 
 
 app = Flask(__name__, static_url_path='')
+
+outPath = '/media/music/rip/Youtube/%(title)s.%(ext)s'
+
 
 
 
@@ -59,65 +26,17 @@ app = Flask(__name__, static_url_path='')
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/get_phonebook')
-def get_telefonBuch():
-    return "".join(["%i - %s<br/>"%(k,telefonBuch[k]) for k in sorted(telefonBuch.keys())])
-
-    return jsonify(telefonBuch)
-
-@app.route('/remove_number/<number>')
-def remove_number(number):
-    try:
-        number=int(number)
-        if telefonBuch.has_key(number):
-            telefonBuch.pop(number,0)
-        else:
-            pass
-    except:
-        pass
-    return app.send_static_file('index.html')
-
-
-
 @app.route('/set_number',methods=['POST'])
 def set_number():
-    try:
-        name=request.form['playlist_input']
-        number=int(request.form['number_input'])
-        print number,name
-        telefonBuch[number]=name
-        save_to_pickle()
-    except:
-        pass
+    youUrl=request.form['youtube_url']
+    print youUrl
+    youtube_dl = "/usr/bin/youtube-dl %s -o %s -f bestaudio" %(youUrl, outPath)
+
+
+    arg_ls = shlex.split(youtube_dl)
+    download_youtube=subprocess.Popen(arg_ls)
+
     return app.send_static_file('index.html')
-
-
-@app.route('/play_number/<number>')
-def play_number(number):
-        number=int(number)
-        if telefonBuch.has_key(number):
-            name=telefonBuch[number]
-            name=name.encode('utf-8',errors='ignore')
-            print name
-            print number
-            try:
-                play_playlist(name)
-            except:
-                return('play fail')
-                pass
-            return name
-
-@app.route('/get_playlists')
-def get_playlists():
-    global playlists
-    try:
-        just_namelist=get_playlists_from_mpd()
-    except:
-        pass
-
-    print(just_namelist)
-    return json.dumps(just_namelist)
-
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0',port=443,ssl_context=context,debug=True)
